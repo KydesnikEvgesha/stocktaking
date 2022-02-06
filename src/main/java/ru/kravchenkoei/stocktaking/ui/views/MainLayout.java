1f212b4ea0072103019ef500302e386eb0081b9f
Evgeny Kravchenko <fizika_t@mail.ru>
2022-02-06 22:04:29 +0500

    Добавил пункты в меню для перехода на страницы

--- src/main/java/ru/kravchenkoei/stocktaking/ui/views/MainLayout.java
+++ src/main/java/ru/kravchenkoei/stocktaking/ui/views/MainLayout.java
@@ -9,45 +9,44 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
-import ru.kravchenkoei.stocktaking.ui.views.list.CompanyListView;
-import ru.kravchenkoei.stocktaking.ui.views.list.ElementListView;
-import ru.kravchenkoei.stocktaking.ui.views.list.TypeListView;
+import ru.kravchenkoei.stocktaking.ui.views.list.*;
 
 @Route("")
 public class MainLayout extends AppLayout {
 
+  public MainLayout() {
+    createHeader();
+    createDrawer();
+  }
 
-    public MainLayout() {
-        createHeader();
-        createDrawer();
-    }
+  private void createHeader() {
+    H2 logo = new H2("Учет офисной техники");
 
-    private void createHeader() {
-        H2 logo = new H2("Список оборудования");
+    HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo);
 
-        HorizontalLayout header = new HorizontalLayout(
-                new DrawerToggle(),
-                logo
-        );
+    header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+    header.expand(logo);
+    header.setWidth("100%");
 
-        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
-        header.expand(logo);
-        header.setWidth("100%");
+    addToNavbar(header);
+  }
 
-        addToNavbar(header);
+  private void createDrawer() {
+    RouterLink employeeListLink = new RouterLink("Сотрудники", EmployeeListView.class);
+    RouterLink companyListLink = new RouterLink("Производители", CompanyListView.class);
+    RouterLink elementListLink = new RouterLink("Оборудование", ElementListView.class);
+    RouterLink typeListLink = new RouterLink("Вид оборудования", TypeListView.class);
+    RouterLink specListLink = new RouterLink("Характеристики", SpecificationListView.class);
+    RouterLink locationListLink = new RouterLink("Площадки", LocationListView.class);
+    companyListLink.setHighlightCondition(HighlightConditions.sameLocation());
 
-    }
-
-    private void createDrawer() {
-        RouterLink companyListLink = new RouterLink("Производители", CompanyListView.class);
-        RouterLink elementListLink = new RouterLink("Оборудование", ElementListView.class);
-        RouterLink typeListLink = new RouterLink("Вид оборудования", TypeListView.class);
-        companyListLink.setHighlightCondition(HighlightConditions.sameLocation());
-
-        addToDrawer(new VerticalLayout(
-                companyListLink,
-                elementListLink,
-                typeListLink
-        ));
-    }
+    addToDrawer(
+        new VerticalLayout(
+            employeeListLink,
+            companyListLink,
+            elementListLink,
+            typeListLink,
+            specListLink,
+            locationListLink));
+  }
 }
