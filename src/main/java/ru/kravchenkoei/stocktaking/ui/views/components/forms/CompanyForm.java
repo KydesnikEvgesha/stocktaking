56a2cd9b1d1bc8ee9b40e45a70614cb5fd4c772c
Evgeny Kravchenko <fizika_t@mail.ru>
2022-02-06 22:01:45 +0500

    Добавил форму ввода/вывода данных по производителям

--- src/main/java/ru/kravchenkoei/stocktaking/ui/views/components/forms/CompanyForm.java
+++ src/main/java/ru/kravchenkoei/stocktaking/ui/views/components/forms/CompanyForm.java
@@ -1,9 +1,6 @@
 package ru.kravchenkoei.stocktaking.ui.views.components.forms;
 
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.ComponentEvent;
-import com.vaadin.flow.component.ComponentEventListener;
-import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.*;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
@@ -16,90 +13,90 @@ import com.vaadin.flow.shared.Registration;
 import ru.kravchenkoei.stocktaking.data.model.Company;
 
 public class CompanyForm extends FormLayout {
+  private Company company;
+  TextField name = new TextField("Название");
+  TextField address = new TextField("Адрес");
+
+  Button save = new Button("Сохранить");
+  Button delete = new Button("Удалить");
+  Button close = new Button("Отменить");
+
+  Binder<Company> binder = new BeanValidationBinder<>(Company.class);
+
+  public CompanyForm() {
+    addClassName("contact-form");
+
+    binder.bindInstanceFields(this);
+    add(name, address, createButtonsLayout());
+  }
+
+  private Component createButtonsLayout() {
+    save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+    delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+    close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+    save.addClickShortcut(Key.ENTER);
+    close.addClickShortcut(Key.ESCAPE);
+
+    save.addClickListener(event -> validateAndSave());
+    delete.addClickListener(event -> fireEvent(new DeleteEvent(this, company)));
+    close.addClickListener(event -> fireEvent(new CloseEvent(this)));
+
+    binder.addStatusChangeListener(e -> save.setEnabled(binder.isValid()));
+    return new HorizontalLayout(save, delete, close);
+  }
+
+  private void validateAndSave() {
+    try {
+      binder.writeBean(company);
+      fireEvent(new SaveEvent(this, company));
+    } catch (ValidationException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void addClickCancelListener(ComponentEventListener<ClickEvent<Button>> clickListener) {
+    close.addClickListener(clickListener);
+  }
+
+  public void setCompany(Company company) {
+    this.company = company;
+    binder.readBean(company);
+  }
+
+  public abstract static class CompanyFormEvent extends ComponentEvent<CompanyForm> {
     private Company company;
-    TextField name = new TextField("Название");
-    TextField address = new TextField("Адрес");
 
-    Button save = new Button("Сохранить");
-    Button delete = new Button("Удалить");
-    Button close = new Button("Отменить");
-
-    Binder<Company> binder = new BeanValidationBinder<>(Company.class);
-
-    public CompanyForm() {
-        addClassName("contact-form");
-
-        binder.bindInstanceFields(this);
-        add(name,
-                address,
-                createButtonsLayout());
+    protected CompanyFormEvent(CompanyForm source, Company company) {
+      super(source, false);
+      this.company = company;
     }
 
-    private Component createButtonsLayout() {
-        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
-        close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-
-        save.addClickShortcut(Key.ENTER);
-        close.addClickShortcut(Key.ESCAPE);
-
-        save.addClickListener(event -> validateAndSave());
-        delete.addClickListener(event -> fireEvent(new DeleteEvent(this, company)));
-        close.addClickListener(event -> fireEvent(new CloseEvent(this)));
-
-        binder.addStatusChangeListener(e -> save.setEnabled(binder.isValid()));
-        return new HorizontalLayout(save, delete, close);
+    public Company getCompany() {
+      return company;
     }
+  }
 
-    private void validateAndSave() {
-        try {
-            binder.writeBean(company);
-            fireEvent(new SaveEvent(this, company));
-        } catch (ValidationException e) {
-            e.printStackTrace();
-        }
+  public static class SaveEvent extends CompanyFormEvent {
+    SaveEvent(CompanyForm source, Company company) {
+      super(source, company);
     }
+  }
 
-    public void setCompany(Company company) {
-        this.company = company;
-        binder.readBean(company);
+  public static class DeleteEvent extends CompanyFormEvent {
+    DeleteEvent(CompanyForm source, Company company) {
+      super(source, company);
     }
+  }
 
-    public static abstract class CompanyFormEvent extends ComponentEvent<CompanyForm> {
-        private Company company;
-
-        protected CompanyFormEvent(CompanyForm source, Company company) {
-            super(source, false);
-            this.company = company;
-        }
-
-        public Company getCompany() {
-            return company;
-        }
+  public static class CloseEvent extends CompanyFormEvent {
+    CloseEvent(CompanyForm source) {
+      super(source, null);
     }
+  }
 
-    public static class SaveEvent extends CompanyFormEvent {
-        SaveEvent(CompanyForm source, Company company) {
-            super(source, company);
-        }
-    }
-
-    public static class DeleteEvent extends CompanyFormEvent {
-        DeleteEvent(CompanyForm source, Company company) {
-            super(source, company);
-        }
-
-    }
-
-    public static class CloseEvent extends CompanyFormEvent {
-        CloseEvent(CompanyForm source) {
-            super(source, null);
-        }
-    }
-
-    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,
-                                                                  ComponentEventListener<T> listener) {
-        return getEventBus().addListener(eventType, listener);
-    }
+  public <T extends ComponentEvent<?>> Registration addListener(
+      Class<T> eventType, ComponentEventListener<T> listener) {
+    return getEventBus().addListener(eventType, listener);
+  }
 }
-
--- src/main/java/ru/kravchenkoei/stocktaking/ui/views/list/CompanyListView.java
+++ src/main/java/ru/kravchenkoei/stocktaking/ui/views/list/CompanyListView.java
@@ -14,106 +14,102 @@ import ru.kravchenkoei.stocktaking.data.service.CompanyService;
 import ru.kravchenkoei.stocktaking.ui.views.MainLayout;
 import ru.kravchenkoei.stocktaking.ui.views.components.forms.CompanyForm;
 
-
-@PageTitle("Производители")
+@PageTitle("Производители оборудования")
 @Route(value = "/company", layout = MainLayout.class)
 public class CompanyListView extends VerticalLayout {
 
-    Grid<Company> companyGrid = new Grid<>();
-    TextField filterText = new TextField();
-    CompanyForm companyForm;
-    CompanyService companyService;
+  Grid<Company> companyGrid = new Grid<>();
+  TextField filterText = new TextField();
+  CompanyForm companyForm;
+  CompanyService companyService;
 
-    public CompanyListView(CompanyService companyService) {
-        this.companyService = companyService;
+  public CompanyListView(CompanyService companyService) {
+    this.companyService = companyService;
 
-        setSpacing(false);
-        setSizeFull();
-        configureGrid();
-        configureForm();
+    setSpacing(false);
+    setSizeFull();
+    configureGrid();
+    configureForm();
+    add(getToolbar(), getContent());
+    updateList();
+    closeEditor();
+  }
 
-        add(getToolbar(), getContent());
-        updateList();
-        closeEditor();
+  private Component getContent() {
+    HorizontalLayout content = new HorizontalLayout(companyGrid, companyForm);
+    content.setFlexGrow(2, companyGrid);
+    content.setFlexGrow(1, companyForm);
+    content.addClassNames("content");
+    content.setSizeFull();
+    return content;
+  }
+
+  private void configureForm() {
+    companyForm = new CompanyForm();
+    companyForm.setWidth("25em");
+    companyForm.addListener(CompanyForm.SaveEvent.class, this::saveCompany);
+    companyForm.addListener(CompanyForm.DeleteEvent.class, this::deleteCompany);
+    companyForm.addListener(CompanyForm.CloseEvent.class, e -> closeEditor());
+  }
+
+  private void saveCompany(CompanyForm.SaveEvent event) {
+    companyService.saveCompany(event.getCompany());
+    updateList();
+    closeEditor();
+  }
+
+  private void deleteCompany(CompanyForm.DeleteEvent event) {
+    companyService.deleteCompany(event.getCompany().getId());
+    updateList();
+    closeEditor();
+  }
+
+  public void editCompany(Company company) {
+    if (company == null) {
+      closeEditor();
+    } else {
+      companyForm.setCompany(company);
+      companyForm.setVisible(true);
+      addClassName("editing");
     }
+  }
 
-    private Component getContent() {
-        HorizontalLayout content = new HorizontalLayout(companyGrid, companyForm);
-        content.setFlexGrow(2, companyGrid);
-        content.setFlexGrow(1, companyForm);
-        content.addClassNames("content");
-        content.setSizeFull();
-        return content;
-    }
+  private void closeEditor() {
+    companyForm.setCompany(null);
+    companyForm.setVisible(false);
+    removeClassName("editing");
+  }
 
-    private void configureForm() {
-        companyForm = new CompanyForm();
-        companyForm.setWidth("25em");
-        companyForm.addListener(CompanyForm.SaveEvent.class, this::saveCompany);
-        companyForm.addListener(CompanyForm.DeleteEvent.class, this::deleteCompany);
-        companyForm.addListener(CompanyForm.CloseEvent.class, e -> closeEditor());
-    }
+  private void addCompany() {
+    companyGrid.asSingleSelect().clear();
+    editCompany(new Company());
+  }
 
-    private void saveCompany(CompanyForm.SaveEvent event) {
-        companyService.saveCompany(event.getCompany());
-        updateList();
-        closeEditor();
-    }
+  public void configureGrid() {
+    companyGrid.addClassName("contact-grid");
+    companyGrid.setSizeFull();
+    companyGrid.addColumn(Company::getName).setHeader("Наименование компании");
+    companyGrid.addColumn(Company::getAddress).setHeader("Адрес компании");
+    companyGrid.getColumns().forEach(col -> col.setAutoWidth(true));
 
-    private void deleteCompany(CompanyForm.DeleteEvent event) {
-        companyService.deleteCompany(event.getCompany().getId());
-        updateList();
-        closeEditor();
-    }
+    companyGrid.asSingleSelect().addValueChangeListener(event -> editCompany(event.getValue()));
+  }
 
-    public void editCompany(Company company) {
-        if (company == null) {
-            closeEditor();
-        } else {
-            companyForm.setCompany(company);
-            companyForm.setVisible(true);
-            addClassName("editing");
-        }
-    }
+  private HorizontalLayout getToolbar() {
+    filterText.setPlaceholder("Фильтр по имени...");
+    filterText.setClearButtonVisible(true);
+    filterText.setValueChangeMode(ValueChangeMode.LAZY);
+    filterText.addValueChangeListener(e -> updateList());
 
-    private void closeEditor() {
-        companyForm.setCompany(null);
-        companyForm.setVisible(false);
-        removeClassName("editing");
-    }
+    Button addCompanyButton = new Button("Добавить компанию");
+    addCompanyButton.addClickListener(click -> addCompany());
 
-    private void addCompany() {
-        companyGrid.asSingleSelect().clear();
-        editCompany(new Company());
-    }
+    HorizontalLayout toolbar = new HorizontalLayout(filterText, addCompanyButton);
+    toolbar.addClassName("toolbar");
+    return toolbar;
+  }
 
-
-    public void configureGrid(){
-        companyGrid.addClassName("contact-grid");
-        companyGrid.setSizeFull();
-        companyGrid.addColumn(Company::getName).setHeader("Наименованик компании");
-        companyGrid.addColumn(Company::getAddress).setHeader("Адрес компании");
-        companyGrid.getColumns().forEach(col -> col.setAutoWidth(true));
-
-        companyGrid.asSingleSelect().addValueChangeListener(event ->
-                editCompany(event.getValue()));
-    }
-
-    private HorizontalLayout getToolbar() {
-        filterText.setPlaceholder("Фильтр по имени...");
-        filterText.setClearButtonVisible(true);
-        filterText.setValueChangeMode(ValueChangeMode.LAZY);
-        filterText.addValueChangeListener(e -> updateList());
-
-        Button addCompanyButton = new Button("Добавить компанию");
-        addCompanyButton.addClickListener(click -> addCompany());
-
-        HorizontalLayout toolbar = new HorizontalLayout(filterText, addCompanyButton);
-        toolbar.addClassName("toolbar");
-        return toolbar;
-    }
-
-    private void updateList() {
-        companyGrid.setItems(companyService.searchCompanies(filterText.getValue()));
-    }
+  private void updateList() {
+    companyGrid.setItems(companyService.searchCompanies(filterText.getValue()));
+  }
 }
